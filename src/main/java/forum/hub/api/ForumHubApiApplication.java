2f5510546b7d24f0fbe1684792f8d9739d111274
Lucas Yago <lucasyago63@gmail.com>
2025-08-16 16:10:08 -0300

    cria projeto inicial

---
+++ src/main/java/forum/hub/api/ForumHubApiApplication.java
@@ -0,0 +1,13 @@
+package forum.hub.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumHubApiApplication.class, args);
+	}
+
+}
